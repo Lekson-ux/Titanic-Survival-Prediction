330fb0c6e99641ebb1339c4f4a5e98ffcad28ed9
Husman <usmanoladapo69@gmail.com>
2022-12-27 13:39:24 -1100

    Changes committed

---
+++ app.py
@@ -0,0 +1,21 @@
+import streamlit as st
+import pandas as pd
+import pickle
+import sklearn
+import streamlit
+import numpy as np
+import pandas as pd
+
+load_model = pickle.load(open('Titanic_model.pkl', 'rb'))
+
+input_data = ([[12, 30.0, 0, 1]])
+
+
+prediction = load_model.predict(input_data)
+print(prediction)
+
+
+if prediction[0] == 0:
+    print("The passenger did not survive.")
+else:
+    print("The passenger survived.")
---
+++ webapp.py
@@ -0,0 +1,51 @@
+import streamlit as st
+import pandas as pd
+import pickle
+
+# Load the trained machine learning model
+model = pickle.load(open("titanic_model.pkl", "rb"))
+
+
+
+import streamlit as st
+import pandas as pd
+import pickle
+
+# Load the trained machine learning model
+model = pickle.load(open("Titanic_model.pkl", "rb"))
+
+
+
+# Create the Streamlit app
+st.title("Titanic Survival Prediction")
+st.subheader('Developer: Usman Oladapo :sunglasses:')
+st.write("### Provide the information below to predict the survival status of passengers in 1912 Titanic ship wreck")
+
+
+# Collect input from the user
+age = st.number_input("Age", min_value=0, max_value=100)
+fare = st.number_input('Passenger Fare(£)', min_value= 0.00, max_value = 1000.00)
+gender = st.radio("Gender", ["Male", "Female"])
+pclass = st.selectbox("Passenger Class", ['1st', '2nd', '3rd'])
+
+# Preprocess the input data
+input_data = pd.DataFrame({"Age": [age], 'Fare': [fare],  "Sex": [gender], "Pclass": [pclass]})
+input_data["Sex"] = input_data["Sex"].map({"Male": 0, "Female": 1})
+input_data["Pclass"] = input_data["Pclass"].map({"1st": 1, "2nd": 2, '3rd': 3})
+
+# Use the model to make a prediction
+prediction = model.predict(input_data)[0]
+
+# Display the prediction to the user
+
+if st.button('Predict survival status'):
+  if prediction == 0:
+    st.write("The passenger did not survive.")
+  else:
+    st.write("The passenger survived.")
+
+
+
+
+
+
